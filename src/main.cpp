a5fb5e2e3f9bb5b4c7f6be173a1fe30693d0240b
SOS1SOS1 <43181395+SOS1SOS1@users.noreply.github.com>
2023-04-07 00:23:44 -0400

    added wifi scanning w/o ble

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,9 @@
 #define WIFI_CHANNEL_SWITCH_INTERVAL  (500)
 #define WIFI_CHANNEL_MAX               (13)
 
+#define DEVICE_TIMEOUT          (1000*60*5) // if a device hasn't been seen for greater than DEVICE_TIMEOUT, clear it
+#define RSSI_THRESHOLD                (-60) // device is considered "in range" if it's RSSI is at or above this
+
 // -- Button vars --
 
 #define BUTTON_PIN 21
@@ -36,20 +39,19 @@ BLEScan* pBLEScan;
 // closer to 0 (zero) the stronger the signal, which means it's better
 int max_RSSI = 1;
 int min_RSSI = 1;
-int rssi_threshold = -65;
 
+// TODO: move to be its own file
+// TODO: might combine with WiFIDetectedDevice to be one class, with property of detected_by maybe
 class BLEDetectedDevice {
   public:
     std::string ble_mac_addr;
     int rssi;
-    unsigned long time_detected;
+    unsigned long time_last_detected;
     uint8_t manufacturer_data[100];
     uint8_t company_identifier[3];
 };
 // DetectedDevice detected_devices[1000];
 LinkedList<BLEDetectedDevice> detected_devices;
-int device_count = 0;
-unsigned long start_time;
 
 void ble_scan_devices();
 int get_in_range_device_count();
@@ -59,16 +61,20 @@ void clear_old_devices();
 // -- Wi-Fi vars --
 // https://github.com/ESP-EOS/ESP32-WiFi-Sniffer/blob/master/WIFI_SNIFFER_ESP32.ino
 
+bool run_wifi_sniff_continous = false;
+
+int max_wifi_RSSI = 1;
+int min_wifi_RSSI = 1;
+
 class WiFiDetectedDevice {
   public:
     std::string wifi_mac_addr;
     int rssi;
-    unsigned long time_detected;
+    unsigned long time_last_detected;
 };
 // DetectedDevice detected_devices[1000];
 LinkedList<WiFiDetectedDevice> sniffer_detected_devices;
-
-bool sniffed_device_before(std::string addr);
+unsigned long last_wifi_sniff_time;
 
 // ? prob won't work if phone's wi-fi is off
 // Might use the BLE functions in parallel to detect further mobiles as a certain part of the BLE MAC address will not change (see my link regarding BLE above).
@@ -77,9 +83,6 @@ bool sniffed_device_before(std::string addr);
 uint8_t level = 0, channel = 1;
 
 static wifi_country_t wifi_country = {.cc="CN", .schan = 1, .nchan = 13}; //Most recent esp32 library struct
-
-// int mac_count = 0;
-// uint8_t mac_lib[300][6];
 
 typedef struct {
   unsigned frame_ctrl:16;
@@ -95,6 +98,11 @@ typedef struct {
   wifi_ieee80211_mac_hdr_t hdr;
   uint8_t payload[0]; /* network data ended with 4 bytes csum (CRC32) */
 } wifi_ieee80211_packet_t;
+
+void wifi_sniff();
+int get_in_range_wifi_device_count();
+void clear_old_wifi_devices();
+bool sniffed_device_before(std::string addr);
 
 static esp_err_t event_handler(void *ctx, system_event_t *event);
 static void wifi_sniffer_init(void);
@@ -115,26 +123,28 @@ class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
         newDevice.ble_mac_addr = strAddrData;
         newDevice.rssi = advertisedDevice.getRSSI();
 
-        // if (advertisedDevice.haveManufacturerData()) {
-        //     std::string strManufacturerData = advertisedDevice.getManufacturerData();
-        //     strManufacturerData.copy((char *)newDevice.manufacturer_data, strManufacturerData.length(), 0);
+        if (advertisedDevice.haveManufacturerData()) {
+            std::string strManufacturerData = advertisedDevice.getManufacturerData();
+            strManufacturerData.copy((char *)newDevice.manufacturer_data, strManufacturerData.length(), 0);
 
-        //     // Company Identifier is the first two bytes in little endian order (0x4C00 -> 0x004C)
-        //     // 4c001006131daf78ab78 --> 0x004C == Apple
-        //     // 100340100230 --> 0x0310 == SGL Italia S.r.l.
-        //     newDevice.company_identifier[1] = newDevice.manufacturer_data[0];
-        //     newDevice.company_identifier[0] = newDevice.manufacturer_data[1];
-        //     // if (cManufacturerData[1] == 0x00 && cManufacturerData[0] == 0x4C) {
-        //     //     Serial.println("apple device");
-        //     //     apple_device_count += 1;
-        //     //     isAppleDevice = true;
-        //     // }
-        // }
+            // Logs device's manufacturer data to serial console
+            // TODO: move to be a function on the BLEDetectedDevice class 
+            // for (int i = 0; i < strManufacturerData.length(); i++) {
+            //     Serial.printf("[%02x]", newDevice.manufacturer_data[i]);
+            // }
+            // Serial.println();
 
-        newDevice.time_detected = millis() - start_time;
-        // detected_devices[device_count] = newDevice;
+            // Company Identifier is the first two bytes in little endian order (0x4C00 -> 0x004C)
+            // 4c001006131daf78ab78 --> 0x004C == Apple
+            // 100340100230 --> 0x0310 == SGL Italia S.r.l.
+            newDevice.company_identifier[1] = newDevice.manufacturer_data[0];
+            newDevice.company_identifier[0] = newDevice.manufacturer_data[1];
+            // TODO: move to be a function on the BLEDetectedDevice class 
+            // Serial.printf("0x%02x%02x\n", newDevice.manufacturer_data[1], newDevice.manufacturer_data[0]);
+        }
+
+        newDevice.time_last_detected = millis();
         detected_devices.add(newDevice);
-        // device_count += 1;
 
         // Serial.printf("%d. Advertised Device: %s \n", device_count, advertisedDevice.toString().c_str());
 
@@ -143,6 +153,7 @@ class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
         // BLE_ADDR_PUBLIC (0x00), BLE_ADDR_RANDOM (0x01), BLE_ADDR_PUBLIC_ID (0x02), BLE_ADDR_RANDOM_ID (0x03)
         // Serial.printf("Address Type: %zu \n", (unsigned int)advertisedDevice.getAddressType());
 
+        // For testing purposes, tracks the max and min RSSI seen
         if (advertisedDevice.getRSSI() > max_RSSI || max_RSSI == 1) {
             max_RSSI = advertisedDevice.getRSSI();
         }
@@ -150,7 +161,6 @@ class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
             min_RSSI = advertisedDevice.getRSSI();
         }
 
-        // bool isAppleDevice = false;
         if (advertisedDevice.haveName()) {
             Serial.print("Device name: ");
             Serial.println(advertisedDevice.getName().c_str());
@@ -163,18 +173,14 @@ class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
         }
         
         // ? could add member service id's to object, help say what company it is
-        // if (advertisedDevice.haveServiceUUID()) {
-        //     std::string res = "";
-            // for (int i=0; i < advertisedDevice.getServiceUUIDCount(); i++) {
-            //     res += ", serviceUUID: " + advertisedDevice.getServiceUUID(i).toString();
-            // }
-            // Serial.print("-- Service UUIDs --  ");
-            // Serial.println(res.c_str());
-            // device_count_with_service_uuid += 1;
-            // if (advertisedDevice.getRSSI() > rssi_threshold) {
-            //     device_count_in_rssi_with_uuid += 1;
-            // }
-        // }
+        if (advertisedDevice.haveServiceUUID()) {
+            std::string res = "";
+            for (int i=0; i < advertisedDevice.getServiceUUIDCount(); i++) {
+                res += ", serviceUUID: " + advertisedDevice.getServiceUUID(i).toString();
+            }
+            Serial.print("-- Service UUIDs --  ");
+            Serial.println(res.c_str());
+        }
 
         // uint8_t *payload = advertisedDevice.getPayload();
         // size_t payloadLength = advertisedDevice.getPayloadLength();
@@ -187,54 +193,75 @@ class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
         // }
         // Serial.println();
 
-        // ? could use to check for a particular advertising service, maybe find one unique to phones
-        // Serial.printf("Advertising Service?: %zu \n", advertisedDevice.isAdvertisingService());
+        // Can be used to check if a particular service is being advertised
+        BLEUUID ble_exposure_notification_uuid = BLEUUID("0000fd6f-0000-1000-8000-00805f9b34fb");
+        if (advertisedDevice.isAdvertisingService(ble_exposure_notification_uuid)) {
+            Serial.println("advertising covid exposure notification service");
+        }
 
     }
 };
 
 void setup() {
     Serial.begin(115200);
+    delay(100);
+
     Serial.println("Scanning...");
-    start_time = millis();
+    last_wifi_sniff_time = millis();
+
+    run_wifi_sniff_continous = true;
 
     // initialize the pushbutton pin as an pull-up input
     pinMode(BUTTON_PIN, INPUT_PULLUP);
 
     // Initializes the ESP32 as a BLE device
-    BLEDevice::init("");
-    pBLEScan = BLEDevice::getScan(); //create new scan
-    pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
-    pBLEScan->setActiveScan(false); //active scan uses more power, but get results faster
-    pBLEScan->setInterval(100); 
-    pBLEScan->setWindow(99);  // less or equal setInterval value
+    // BLEDevice::init("");
+    // pBLEScan = BLEDevice::getScan(); //create new scan
+    // pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+    // pBLEScan->setActiveScan(false); //active scan uses more power, but get results faster
+    // pBLEScan->setInterval(100); 
+    // pBLEScan->setWindow(99);  // less or equal setInterval value
 
     // Initializes Wi-Fi Sniffer
-    // delay(10);
-    // wifi_sniffer_init();
+    delay(10);
+    wifi_sniffer_init();
 }
 
 void loop() {
-
-    //Serial.print("inside loop");
+    // Serial.print("inside loop");
     // delay(1000); // wait for a second
     
-    // vTaskDelay(WIFI_CHANNEL_SWITCH_INTERVAL / portTICK_PERIOD_MS);
-    // wifi_sniffer_set_channel(channel);
-    // channel = (channel % WIFI_CHANNEL_MAX) + 1;
-
-    if (run_ble_continous) {
-        // Scan every 30 seconds
-        ble_scan_devices();
-        delay(1000 * 30); // TODO: change to be non-blocking
-    } else {
-        // Scan when button is pressed
-        currentState = digitalRead(BUTTON_PIN);
-        if(lastState == LOW && currentState == HIGH && scanning == false) {
-            ble_scan_devices();
-        }
-        lastState = currentState;
+    bool button_pressed = false;
+    currentState = digitalRead(BUTTON_PIN);
+    if(lastState == LOW && currentState == HIGH && scanning == false) {
+        Serial.println("button pressed");
+        button_pressed = true;
     }
+    lastState = currentState;
+
+    if (run_wifi_sniff_continous) {
+        // Perform wifi sniffing every 3 minutes
+        if (millis() - last_wifi_sniff_time > 1000*60*3) {
+            last_wifi_sniff_time = millis();
+            wifi_sniff();
+        }
+    } else if (button_pressed) {
+        // Perform wifi sniffing every time the button is pressed
+        wifi_sniff();
+    }
+
+    // if (run_ble_continous) {
+    //     // Scan every 30 seconds
+    //     ble_scan_devices();
+    //     delay(1000 * 30); // TODO: change to be non-blocking
+    // } else {
+    //     // Scan when button is pressed
+    //     currentState = digitalRead(BUTTON_PIN);
+    //     if(lastState == LOW && currentState == HIGH && scanning == false) {
+    //         ble_scan_devices();
+    //     }
+    //     lastState = currentState;
+    // }
 
 }
 
@@ -242,10 +269,10 @@ void ble_scan_devices() {
     // put your main code here, to run repeatedly:
     scanning = true;
 
-    // TODO: Clear out old devices from detected devices list
+    // Clear out devices that haven't been seen recently
     clear_old_devices();
 
-    Serial.print("Starting scan... ");
+    Serial.println("Starting scan... ");
     BLEScanResults foundDevices = pBLEScan->start(scanTime, false);
     // Serial.print("\nDevices found: ");
     // Serial.println(foundDevices.getCount());
@@ -264,7 +291,7 @@ void ble_scan_devices() {
 int get_in_range_device_count() {
     int count = 0;
     for (int i = 0; i < detected_devices.size(); i++) {
-        if (detected_devices.get(i).rssi > rssi_threshold) {
+        if (detected_devices.get(i).rssi > RSSI_THRESHOLD) {
             count += 1;
         }
     }
@@ -274,6 +301,9 @@ int get_in_range_device_count() {
 bool device_seen_before(std::string addr) {
     for (int i = 0; i < detected_devices.size(); i++) {
         if (detected_devices.get(i).ble_mac_addr.compare(addr) == 0) {
+            // Update time last detected
+            BLEDetectedDevice device = detected_devices.get(i);
+            device.time_last_detected = millis();
             return true;
         }
     }
@@ -283,7 +313,8 @@ bool device_seen_before(std::string addr) {
 void clear_old_devices() {
     for (int i = 0; i < detected_devices.size(); i++) {
         // If device not seen in the last minute, remove it
-        if (detected_devices.get(i).time_detected > 1000*60) {
+        if (millis() - detected_devices.get(i).time_last_detected > DEVICE_TIMEOUT) {
+            Serial.println("cleared an old device");
             detected_devices.remove(i);
         }
     }
@@ -303,7 +334,7 @@ void wifi_sniffer_init(void) {
   ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
   ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_NULL) );
   ESP_ERROR_CHECK( esp_wifi_start() );
-  esp_wifi_set_promiscuous(true);
+//   esp_wifi_set_promiscuous(true); // appears to turn on the wifi scanning
   esp_wifi_set_promiscuous_rx_cb(&wifi_sniffer_packet_handler);
 }
 
@@ -313,7 +344,7 @@ void wifi_sniffer_set_channel(uint8_t channel) {
 
 const char * wifi_sniffer_packet_type2str(wifi_promiscuous_pkt_type_t type) {
   switch(type) {
-    case WIFI_PKT_MGMT: return "MGMT";
+    case WIFI_PKT_MGMT: return "MGMT"; // only ever seen this packet type so far
     case WIFI_PKT_DATA: return "DATA";
     default:  
     case WIFI_PKT_MISC: return "MISC";
@@ -329,51 +360,108 @@ void wifi_sniffer_packet_handler(void* buff, wifi_promiscuous_pkt_type_t type) {
     const wifi_ieee80211_packet_t *ipkt = (wifi_ieee80211_packet_t *)ppkt->payload;
     const wifi_ieee80211_mac_hdr_t *hdr = &ipkt->hdr;
 
-    Serial.println();
-
-    // only examine packet if within a certain RSSI threshold
-    // if (ppkt->rx_ctrl.rssi < rssi_threshold)
-
-    char addr[15];
+    char addr[20];
     sprintf(addr, "%02x:%02x:%02x:%02x:%02x:%02x", hdr->addr2[0],hdr->addr2[1],hdr->addr2[2], hdr->addr2[3],hdr->addr2[4],hdr->addr2[5]);
     std::string mac_addr = std::string(addr);
+    // Serial.println(mac_addr.c_str());
+    // be:d7:d4:e4:81:9�␞�?␁
 
     // Looks at sender address in packet
-    if (sniffed_device_before(mac_addr))
+    if (sniffed_device_before(mac_addr)) {
+        // Serial.println("device seen before");
         return;
+    }
+
+    // For testing purposes, tracks the max and min RSSI seen
+    if (ppkt->rx_ctrl.rssi > max_wifi_RSSI || max_wifi_RSSI == 1) {
+        max_wifi_RSSI = ppkt->rx_ctrl.rssi;
+    }
+    if (ppkt->rx_ctrl.rssi < min_wifi_RSSI || min_wifi_RSSI == 1) {
+        min_wifi_RSSI = ppkt->rx_ctrl.rssi;
+    }
 
     // only log new found mac devices
-    printf("from addr2 PACKET TYPE=%s, CHAN=%02d, RSSI=%02d,"
-    " ADDR1=%02x:%02x:%02x:%02x:%02x:%02x,"
-    " ADDR2=%02x:%02x:%02x:%02x:%02x:%02x,"
-    " ADDR3=%02x:%02x:%02x:%02x:%02x:%02x\n",
-    wifi_sniffer_packet_type2str(type),
-    ppkt->rx_ctrl.channel,
-    ppkt->rx_ctrl.rssi,
-    /* ADDR1 */
-    hdr->addr1[0],hdr->addr1[1],hdr->addr1[2],
-    hdr->addr1[3],hdr->addr1[4],hdr->addr1[5],
-    /* ADDR2 */
-    hdr->addr2[0],hdr->addr2[1],hdr->addr2[2],
-    hdr->addr2[3],hdr->addr2[4],hdr->addr2[5],
-    /* ADDR3 */
-    hdr->addr3[0],hdr->addr3[1],hdr->addr3[2],
-    hdr->addr3[3],hdr->addr3[4],hdr->addr3[5]
-    );
+    // printf("from addr2 PACKET TYPE=%s, CHAN=%02d, RSSI=%02d,"
+    // " ADDR1=%02x:%02x:%02x:%02x:%02x:%02x,"
+    // " ADDR2=%02x:%02x:%02x:%02x:%02x:%02x,"
+    // " ADDR3=%02x:%02x:%02x:%02x:%02x:%02x\n",
+    // wifi_sniffer_packet_type2str(type),
+    // ppkt->rx_ctrl.channel,
+    // ppkt->rx_ctrl.rssi,
+    // /* ADDR1 */
+    // hdr->addr1[0],hdr->addr1[1],hdr->addr1[2],
+    // hdr->addr1[3],hdr->addr1[4],hdr->addr1[5],
+    // /* ADDR2 */
+    // hdr->addr2[0],hdr->addr2[1],hdr->addr2[2],
+    // hdr->addr2[3],hdr->addr2[4],hdr->addr2[5],
+    // /* ADDR3 */
+    // hdr->addr3[0],hdr->addr3[1],hdr->addr3[2],
+    // hdr->addr3[3],hdr->addr3[4],hdr->addr3[5]
+    // );
 
     // Add new detected device to linked list
     WiFiDetectedDevice newDevice;
     newDevice.wifi_mac_addr = mac_addr;
     newDevice.rssi = ppkt->rx_ctrl.rssi;
-    newDevice.time_detected = millis() - start_time;
+    newDevice.time_last_detected = millis();
     sniffer_detected_devices.add(newDevice);
+}
+
+void wifi_sniff() {
+    // Clear out old devices detected from wifi scanning that haven't been seen recently
+    clear_old_wifi_devices();
+
+    // Perform wifi sniffing for 3 seconds per channel
+    esp_wifi_set_promiscuous(true);
+
+    for (int i = 1; i < WIFI_CHANNEL_MAX; i++) {
+        vTaskDelay(WIFI_CHANNEL_SWITCH_INTERVAL / portTICK_PERIOD_MS);
+        wifi_sniffer_set_channel(i);
+        // channel = (channel % WIFI_CHANNEL_MAX) + 1;
+
+        delay(3000);
+    }
+
+    esp_wifi_set_promiscuous(false);
+
+    Serial.print("\nMax Wifi RSSI: ");
+    Serial.print(max_wifi_RSSI);
+    Serial.print("  --  Min Wifi RSSI: ");
+    Serial.print(min_wifi_RSSI);
+    Serial.print("  --  Wifi In Range Device Count: ");
+    Serial.printf("%d, %d\n", get_in_range_wifi_device_count(), sniffer_detected_devices.size());
+    Serial.println("Scan done!");
+    scanning = false;
 }
 
 bool sniffed_device_before(std::string addr) {
     for (int i = 0; i < sniffer_detected_devices.size(); i++) {
         if (sniffer_detected_devices.get(i).wifi_mac_addr.compare(addr) == 0) {
+            // Update time last detected
+            WiFiDetectedDevice device = sniffer_detected_devices.get(i);
+            device.time_last_detected = millis();
             return true;
         }
     }
     return false;
+}
+
+void clear_old_wifi_devices() {
+    for (int i = 0; i < sniffer_detected_devices.size(); i++) {
+        // If device not seen in the last minute, remove it
+        if (millis() - sniffer_detected_devices.get(i).time_last_detected > DEVICE_TIMEOUT) {
+            Serial.println("cleared an old device");
+            sniffer_detected_devices.remove(i);
+        }
+    }
+}
+
+int get_in_range_wifi_device_count() {
+    int count = 0;
+    for (int i = 0; i < sniffer_detected_devices.size(); i++) {
+        if (sniffer_detected_devices.get(i).rssi > RSSI_THRESHOLD) {
+            count += 1;
+        }
+    }
+    return count;
 }
